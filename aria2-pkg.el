33917d3fed57569f3b65681d5f18fc6dac2f5498
LdBeth <andpuke@foxmail.com>
2017-02-10 10:30:12 +0800

    init dic

---
+++ aria2-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "aria2" "20141107.1517" "Control aria2c commandline tool from Emacs" '((emacs "24.4")) :url "https://bitbucket.org/ukaszg/aria2-mode" :keywords '("download" "bittorrent" "aria2"))
