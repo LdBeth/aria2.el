958b01c4d08c16782c21091401df27afb594b50e
LdBeth <andpuke@foxmail.com>
2017-04-27 01:33:00 +0800

    Delete aria2-autoloads.el

--- aria2-autoloads.el
+++
@@ -1,21 +0,0 @@
-;;; aria2-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "aria2" "aria2.el" (22685 5858 0 0))
-;;; Generated autoloads from aria2.el
-
-(autoload 'aria2-downloads-list "aria2" "\
-Display aria2 downloads list.  Enable `aria2-mode' to controll the process.
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; aria2-autoloads.el ends here
